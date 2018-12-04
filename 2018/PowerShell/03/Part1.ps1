f7ee0dc02bf56f37285be649bb88ce58af8ea079
Anthony Allen <losthammer@gmail.com>
2018-12-04 22:56:33 +0100

    Add day3 part 2

--- 2018/PowerShell/03/Part1.ps1
+++ 2018/PowerShell/03/Part1.ps1
@@ -1,37 +1,47 @@
 $exInput = Get-Content $PsScriptRoot\..\..\Input\Day3.txt
 
-$claims = [System.Collections.Generic.List[PSObject]]::New()
+$uniqueClaims = [System.Collections.Generic.List[int]]::New()
 
-[string[, ]]$grid = [string[, ]]::new(1000, 1000)
+$grid = [int[, ]]::new(1000, 1000)
 $doubleClaimed = 0
 
 foreach ($i in $exInput) {
-    $strMatch = [regex]::Match($i, "(#\d+) @ (\d+),(\d+): (\d+)x(\d+)")
+    $strMatch = [regex]::Match($i, "#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
     $claim = [PSCustomObject]@{
-        Id        = $strMatch.Groups[1].Value
+        Id        = [int]$strMatch.Groups[1].Value
         XLocation = [int]$strMatch.Groups[2].Value
         YLocation = [int]$strMatch.Groups[3].Value
         XSize     = [int]$strMatch.Groups[4].Value
         YSize     = [int]$strMatch.Groups[5].Value
     }
 
+    $uniqueClaims.add($claim.Id)
+    
     for ($i = $claim.XLocation; $i -lt $claim.XSize + $claim.xLocation; $i++) {
         for ($j = $claim.YLocation; $j -lt $claim.YSize + $claim.YLocation; $j++) {
+           
+            $currentGridValue = $grid[$i, $j]
             
-            switch -Regex ($grid[$i, $j]) {
-                "^#" {
-                    $doubleClaimed++
-                    $grid[$i, $j] = 'X' 
+            switch ($currentGridValue) {
+                -1 {
+                    [void]$uniqueClaims.Remove($claim.Id)
                 }
-                "^X" {
-                    # Do nothing
+                0 {
+                    $grid[$i, $j] = $claim.Id
                 }
                 Default {
-                    $grid[$i, $j] = $claim.Id
+                    $doubleClaimed++
+                    [void]$uniqueClaims.Remove($currentGridValue)
+                    [void]$uniqueClaims.Remove($claim.Id)
+                    $grid[$i, $j] = -1 
                 }
             }
         }
     }
+
 }
 
-$doubleClaimed
+[PSCustomObject]@{
+    DoubleClaimed = $doubleClaimed
+    UniqueClaim  = $uniqueClaims[0]
+}
