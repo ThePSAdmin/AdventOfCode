5743ffd6210ffe4d66e8197b1578977b47a50957
Anthony Allen <losthammer@gmail.com>
2018-12-12 08:08:56 +0100

    Solve day 7 part 2

--- 2018/PowerShell/07/Day7.tests.ps1
+++ 2018/PowerShell/07/Day7.tests.ps1
@@ -1,9 +1,13 @@
 Describe "Day 7 tests" {
     $sampleInput = Get-Content $PSScriptRoot\SampleInput.txt
 
-    $result = & $PSScriptRoot\Solution.ps1 -PuzzleInput $sampleInput
+    $result = & $PSScriptRoot\Solution.ps1 -PuzzleInput $sampleInput -StepDuration 0 -NumWorkers 5
 
     it "Should solve Day 7 Part 1 with sample input" {
         $Result.Part1 | Should -Be "CABDFE"
+    }
+
+    it "Should solve Day 7 Part 2 with sample input" {
+        $Result.Part2 | Should -Be 15
     }
 }
--- 2018/PowerShell/07/Solution.ps1
+++ 2018/PowerShell/07/Solution.ps1
@@ -2,8 +2,18 @@
 param(
     [Parameter()]
     [string[]]
-    $PuzzleInput
+    $PuzzleInput,
+
+    [Parameter()]
+    [int]
+    $StepDuration,
+
+    [Parameter()]
+    [int]
+    $NumWorkers
 )
+
+# This really needs to be refactored, but I just wanted to solve the puzzle as I'm already 4 days behind....
 
 $ht = @{}
 
@@ -26,6 +36,8 @@ foreach ($i in $ht.Clone().GetEnumerator()) {
     }
 }
 
+$part2Ht = $Ht.clone()
+
 $outString = ""
 while ($ht.Keys.Count -ne 0) {
     foreach ($h in $ht.Keys | Sort-Object) {
@@ -42,6 +54,52 @@ while ($ht.Keys.Count -ne 0) {
     }
 }
 
+#part 2
+$totalSecondsElapsed = 0
+$availableChars = [char[]]::New(0)
+$workers = [PSCustomObject[]]::New($NumWorkers)
+$part2OutString = ""
+while ($part2Ht.Keys.Count -ne 0 -or (($workers | Where-Object {$_.IsBusy}))) {
+    #Finds free chars to start work on.
+    foreach ($h in $part2Ht.Keys | Sort-Object) {
+        if ($part2Ht[$h].Count -eq 0) {
+            $availableChars += $h
+        }
+    }
+
+    foreach ($availableChar in $availableChars) {
+        for ($j = 0; $j -lt $workers.count; $j++) {
+            if (-not $workers[$j].IsBusy) {
+                $workers[$j] = [PSCustomObject]@{
+                    WorkerChar     = [char]$availableChar
+                    ElapsedSeconds = 1
+                    IsBusy         = $true
+                }
+                $availableChars = $availableChars -ne $availableChar
+                $part2ht.Remove($availableChar)
+                break
+            }
+        }
+    }
+
+    foreach ($worker in $workers | Where-Object {$_.IsBusy}) {
+        Write-Verbose "Char: $($worker.WorkerChar) Should complete after $(([int]$worker.WorkerChar - 64) + $StepDuration). Elapsed Seconds: $($worker.ElapsedSeconds)"
+        if (([int]$worker.WorkerChar - 64) + $StepDuration -le $worker.ElapsedSeconds) {
+            $part2OutString += $worker.WorkerChar
+            foreach ($char in $($part2ht.Keys)) {
+                $part2ht[$char] = $part2ht[$char] -ne $worker.WorkerChar
+            }
+    
+            $worker.IsBusy = $false
+        } else {
+            $worker.ElapsedSeconds++
+        }
+    }
+
+    $totalSecondsElapsed++
+}
+
 [PsCustomObject]@{
     Part1 = $outString
-}
+    Part2 = $totalSecondsElapsed
+}  
