aba8d4e08c42481b42df5db0c3dd1064274d4cde
Anthony Allen <anthony.allen@snowsoftware.com>
2020-01-05 17:31:56 +0100

    2019 day 1 scratch

---
+++ 2019/fsharp/01/scratch.fsx
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+
+let fuelRequired mass =
+  mass / 3 - 2
+
+let input =
+  File.ReadAllLines(@"C:\Temp\aoc01.txt")
+    |> Seq.map (fun s -> s |> int)
+
+let fuel1 = 
+    input 
+     |> Seq.sumBy fuelRequired
+
+let fuel2 mass =
+    let rec calcFuel fuel =
+      if fuel <= 0 then 0
+      else fuel + calcFuel (fuelRequired fuel)
+    calcFuel (fuelRequired mass)
+
+let part2 = 
+    input 
+        |> Seq.sumBy fuel2
+  
