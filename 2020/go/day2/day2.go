f859cf288a43d9d5887c667d19a52e30620b574d
Anthony Allen <anthony.allen@snowsoftware.com>
2020-12-03 23:14:56 +0100

    Solve day 2

---
+++ 2020/go/day2/day2.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/thepsadmin/adventofcode/2020/go/utils"
+)
+
+type Password struct {
+	Min      int
+	Max      int
+	Char     byte
+	Password []byte
+}
+
+func parsePassword(s string) Password {
+	r := regexp.MustCompile(`(\d+)-(\d+) (\w): (\w*)`)
+	p := r.FindStringSubmatch(s)
+	min, _ := strconv.Atoi(p[1])
+	max, _ := strconv.Atoi(p[2])
+	char := byte(p[3][0])
+	password := []byte(p[4])
+	return Password{
+		Min:      min,
+		Max:      max,
+		Char:     char,
+		Password: password,
+	}
+}
+
+func main() {
+	fmt.Println(Part1(utils.ParseFileS("../inputs/day2")))
+	fmt.Println(Part2(utils.ParseFileS("../inputs/day2")))
+}
+
+func Part1(input []string) int {
+	result := 0
+	for _, v := range input {
+		if isValidPassword(parsePassword(v)) {
+			result = result + 1
+		}
+	}
+	return result
+}
+
+func Part2(input []string) int {
+	result := 0
+	for _, v := range input {
+		p := parsePassword(v)
+		var first bool
+		var second bool
+		if p.Password[p.Min-1] == p.Char {
+			first = true
+		}
+
+		if p.Password[p.Max-1] == p.Char {
+			second = true
+		}
+
+		if first != second {
+			result = result + 1
+		}
+	}
+	return result
+}
+
+func isValidPassword(p Password) bool {
+	count := 0
+	for _, v := range p.Password {
+		if v == p.Char {
+			count = count + 1
+		}
+	}
+	if count >= p.Min && count <= p.Max {
+		return true
+	}
+	return false
+}
--- 2020/go/utils/utils.go
+++ 2020/go/utils/utils.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"bufio"
+	"os"
+)
+
+// ParseFileS parses a file and returns a slice of strings
+func ParseFileS(filename string) []string {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	var r []string
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		r = append(r, scanner.Text())
+	}
+	return r
+}
